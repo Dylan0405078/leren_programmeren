874b2ba591df6888da14caf1b84752ee98d9a995
dylan0405078 <99078040@mydavinci.nl>
2023-10-26 09:36:45 +0200

    edits
    edits

--- sollicitatie.py
+++ sollicitatie.py
@@ -1,8 +1,7 @@
 #sollicitatie
 
 
-
-# Definieer constanten voor de vereisten
+# Definieer constanten voor de initiële vereisten
 MIN_GEWICHT = 90  # Minimaal gewicht in kg
 MAX_GEWICHT = 120  # Maximaal gewicht in kg
 MIN_LENGTE = 150  # Minimale lengte in cm
@@ -11,7 +10,7 @@ MIN_DIEREN_ERVARING = 4  # Minimale jaren ervaring met dieren-dressuur
 MIN_JONGLEREN_ERVARING = 5  # Minimale jaren ervaring met jongleren
 MIN_ACROBATIEK_ERVARING = 3  # Minimale jaren ervaring met acrobatiek
 
-# Vraag alle benodigde informatie aan de kandidaat
+# Vraag alle benodigde informatie aan de kandidaat voor de initiële vereisten
 vrachtwagen_rijbewijs = input("Heeft u een geldig Vrachtwagen rijbewijs? (ja/nee): ").strip().lower()
 hoge_hoed = input("Heeft u een hoge hoed? (ja/nee): ").strip().lower()
 lichaamsgewicht = float(input("Wat is uw lichaamsgewicht in kg?: "))
@@ -20,8 +19,8 @@ dieren_erf = int(input("Hoeveel jaar praktijkervaring heeft u met dieren-dressuu
 jongleren_erf = int(input("Hoeveel jaar ervaring heeft u met jongleren?: "))
 acrobatiek_erf = int(input("Hoeveel jaar praktijkervaring heeft u met acrobatiek?: "))
 
-# Beoordeel de geschiktheid van de kandidaat
-geschikt = (
+# Beoordeel de geschiktheid van de kandidaat voor de initiële vereisten
+geschikt_initieel = (
     vrachtwagen_rijbewijs == "ja" and
     hoge_hoed == "ja" and
     MIN_GEWICHT < lichaamsgewicht < MAX_GEWICHT and
@@ -29,8 +28,74 @@ geschikt = (
     (dieren_erf >= MIN_DIEREN_ERVARING or jongleren_erf >= MIN_JONGLEREN_ERVARING or acrobatiek_erf >= MIN_ACROBATIEK_ERVARING)
 )
 
+# Definieer constanten voor de nieuwe vereisten
+MIN_ONDERNEMERS_JAREN = 3
+MIN_WERKNEMERS = 5
+MIN_SNOR_BREEDTE = 10
+MIN_KRULHAAR_LENGTE = 20
+MIN_GLIMLACH_BREEDTE = 10
+
+# Vraag alle benodigde informatie aan de kandidaat voor de nieuwe vereisten
+diploma_ondernemen = input("Heeft u een Diploma MBO-4 ondernemen? (ja/nee): ").strip().lower()
+ondernemer_jaren = int(input("Hoeveel jaar bent u al ondernemer?: "))
+werknemers_in_loondienst = int(input("Hoeveel werknemers heeft u in loondienst?: "))
+geslacht = input("Wat is uw geslacht? (man/vrouw/anders): ").strip().lower()
+
+# Beoordeel de geschiktheid van de kandidaat voor de nieuwe vereisten
+geschikt_nieuw = False
+
+if (
+    (diploma_ondernemen == "ja") or
+    (ondernemer_jaren > MIN_ONDERNEMERS_JAREN and werknemers_in_loondienst >= MIN_WERKNEMERS)
+):
+    if (
+        (geslacht == "man" and input("Heeft u een snor? (ja/nee): ").strip().lower() == "ja" and
+         float(input("Hoe breed is uw snor in cm?: ")) > MIN_SNOR_BREEDTE) or
+        (geslacht == "vrouw" and input("Heeft u rood krulhaar? (ja/nee): ").strip().lower() == "ja" and
+         float(input("Hoe lang is uw rood krulhaar in cm?: ")) > MIN_KRULHAAR_LENGTE) or
+        (geslacht == "anders" and
+         float(input("Hoe breed is uw glimlach in cm?: ")) > MIN_GLIMLACH_BREEDTE)
+    ):
+        geschikt_nieuw = True
+
+# Beoordeel de totale geschiktheid van de kandidaat
+geschikt_totaal = geschikt_initieel and geschikt_nieuw
+
+# Maak een lijst met criteria waaraan niet is voldaan
+criteria_niet_voldaan = []
+
+if not vrachtwagen_rijbewijs == "ja":
+    criteria_niet_voldaan.append("Geldig Vrachtwagen rijbewijs")
+
+if not hoge_hoed == "ja":
+    criteria_niet_voldaan.append("Hoge hoed")
+
+if not (MIN_GEWICHT < lichaamsgewicht < MAX_GEWICHT):
+    criteria_niet_voldaan.append("Geldig lichaamsgewicht")
+
+if not (MIN_LENGTE < lichaamslengte < MAX_LENGTE):
+    criteria_niet_voldaan.append("Geldig lichaamslengte")
+
+if not (dieren_erf >= MIN_DIEREN_ERVARING or jongleren_erf >= MIN_JONGLEREN_ERVARING or acrobatiek_erf >= MIN_ACROBATIEK_ERVARING):
+    criteria_niet_voldaan.append("Ervaring met dieren-dressuur, jongleren of acrobatiek")
+
+if not (diploma_ondernemen == "ja" or (ondernemer_jaren > MIN_ONDERNEMERS_JAREN and werknemers_in_loondienst >= MIN_WERKNEMERS)):
+    criteria_niet_voldaan.append("Diploma MBO-4 ondernemen of ondernemerservaring met minimaal 5 werknemers in loondienst")
+
+if geslacht == "man" and not (input("Heeft u een snor? (ja/nee): ").strip().lower() == "ja" and float(input("Hoe breed is uw snor in cm?: ")) > MIN_SNOR_BREEDTE):
+    criteria_niet_voldaan.append("Snor breder dan 10 cm")
+
+if geslacht == "vrouw" and not (input("Heeft u rood krulhaar? (ja/nee): ").strip().lower() == "ja" and float(input("Hoe lang is uw rood krulhaar in cm?: ")) > MIN_KRULHAAR_LENGTE):
+    criteria_niet_voldaan.append("Rood krulhaar langer dan 20 cm")
+
+if geslacht == "anders" and not (float(input("Hoe breed is uw glimlach in cm?: ")) > MIN_GLIMLACH_BREEDTE):
+    criteria_niet_voldaan.append("Brede glimlach breder dan 10 cm")
+
 # Geef de uitslag van de sollicitatie
-if geschikt:
+if geschikt_totaal:
     print("U mag solliciteren naar de functie van circusdirecteur!")
 else:
     print("U voldoet niet aan alle vereisten om te solliciteren naar de functie van circusdirecteur.")
+    print("Criteria waaraan niet is voldaan:")
+    for criteria in criteria_niet_voldaan:
+        print(criteria)
---
+++ studie advies.py
@@ -0,0 +1,89 @@
+# Definieer constanten voor de teksten en stellingen
+STUDIEDOKTER_TITEL = '''
+*********************** STUDIEADVIES ***********************
+Ik ga jou helpen met jouw opleiding. Jij krijgt een aantal stellingen te zien.
+Voor elke stelling moet je zeggen hoeveel dat bij jou voorkomt. Je kunt steeds 
+antwoorden: 0 is 'altijd'; 1 is 'vaak'; 2 is 'regelmatig'; 3 is 'soms'; 4 is 'nooit'.
+Het is belangrijk om eerlijk te zijn. Op basis van jouw antwoorden krijg je advies. 
+'''
+OPTIES = "Kies 0: altijd; 1: vaak; 2: regelmatig; 3: soms; 4: nooit"
+AANTAL_WEKEN_VRAAG = 'Hoeveel weken ben je al bezig met de opleiding?'
+COMPETENTIE_STELLING_1 = 'Ik voel stress of blokkades bij het maken van programmeeropdrachten.'
+COMPETENTIE_STELLING_2 = 'Ik stel programmeeropdrachten en -uitdagingen uit.'
+COMPETENTIE_STELLING_3 = 'Ik denk dat ik geen talent heb voor de opleiding.'
+COMPETENTIE_STELLING_4 = 'Ik vermijd assessments (CJV) en feedback van kritische docenten. '
+COMPETENTIE_STELLING_5 = 'Ik vergelijk mezelf met anderen die beter lijken te zijn.'
+COMPETENTIE_STELLING_6 = 'Ik voel geen interesse in nieuwe programmeertechnieken.'
+COMPETENTIE_STELLING_7 = 'Ik kopieer code van anderen en lever dat in alsof het helemaal van mij is.'
+
+COMPETENTIE_ADVIES_TITEL = '''
+*********************** STUDIEADVIES ***********************'''
+COMPETENTIE_ADVIES_ZORGELIJK = '''
+Het lijkt erop dat je nog weinig zelfvertrouwen, voldoening en plezier ervaart 
+in het leren programmeren. Er zijn altijd mogelijkheden om je te helpen de draad 
+op te pakken met de opleiding. Praat met jouw SLB-er over extra begeleiding en oefeningen.
+'''
+COMPETENTIE_ADVIES_TWIJFELACHTIG = '''
+Het lijkt erop dat je af en toe weinig zelfvertrouwen, voldoening of plezier ervaart
+in het leren programmeren. Houd je zelfvertrouwen en motivatie in de gaten!
+'''
+COMPETENTIE_ADVIES_GERUSTSTELLEND = '''
+Het lijkt erop dat je voldoende zelfvertrouwen, voldoening en plezier ervaart in
+het leren programmeren. Mocht het een keer minder gaan, maak je geen zorgen. Have fun!
+'''
+
+# Vraag het aantal weken dat de gebruiker al bezig is
+aantal_weken = int(input(AANTAL_WEKEN_VRAAG))
+
+# Stel de stellingen op basis van het aantal weken
+if aantal_weken >= 10:
+    stellingen = [
+        COMPETENTIE_STELLING_1,
+        COMPETENTIE_STELLING_2,
+        COMPETENTIE_STELLING_3,
+        COMPETENTIE_STELLING_4,
+        COMPETENTIE_STELLING_5,
+        COMPETENTIE_STELLING_6,
+        COMPETENTIE_STELLING_7,
+    ]
+else:
+    stellingen = [
+        COMPETENTIE_STELLING_1,
+        COMPETENTIE_STELLING_2,
+        COMPETENTIE_STELLING_3,
+        COMPETENTIE_STELLING_4,
+        COMPETENTIE_STELLING_5,
+    ]
+
+# Initialiseer variabelen voor het bijhouden van antwoorden
+totaal_score = 0
+aantal_altijd_vaak = 0
+aantal_altijd_vaak_regelmatig = 0
+
+# Vraag de gebruiker om antwoorden op de stellingen
+for i, stelling in enumerate(stellingen, start=1):
+    print(f"\nStelling {i}: {stelling}")
+    print(OPTIES)
+    antwoord = int(input("Jouw antwoord (0/1/2/3/4): "))
+    
+    totaal_score += antwoord
+    
+    if antwoord <= 1:
+        aantal_altijd_vaak += 1
+        if antwoord <= 2:
+            aantal_altijd_vaak_regelmatig += 1
+
+# Bereken de gemiddelde score
+gemiddelde_score = totaal_score / len(stellingen)
+
+# Bepaal het advies op basis van de gemiddelde score en antwoorden
+if gemiddelde_score <= 2 or aantal_altijd_vaak > len(stellingen) / 2:
+    advies = COMPETENTIE_ADVIES_ZORGELIJK
+elif gemiddelde_score <= 3 or aantal_altijd_vaak_regelmatig > len(stellingen) / 2:
+    advies = COMPETENTIE_ADVIES_TWIJFELACHTIG
+else:
+    advies = COMPETENTIE_ADVIES_GERUSTSTELLEND
+
+# Toon het advies aan de gebruiker
+print(COMPETENTIE_ADVIES_TITEL)
+print(advies)
