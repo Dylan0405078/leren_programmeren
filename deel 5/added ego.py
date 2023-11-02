b8ec335ee9fcc08d18c347591f2132a8f1f2f21a
dylan0405078 <99078040@mydavinci.nl>
2023-11-02 11:52:08 +0100

    eindverandering
    eindverandering

--- deel 5/added ego.py
+++ deel 5/added ego.py
@@ -1,7 +1,6 @@
 #ego centrische brief
 
 import re
-
 # deelzinnen uit een tekst 
 def get_deelzinnen(text):
    
@@ -13,7 +12,8 @@ def get_deelzinnen(text):
     return sub_sentences
 
 #  ego-score  berekenen
-def calculate_ego_score(sub_sentences):
+def calculate_ego_score(text):
+    sub_sentences = get_deelzinnen(text)
     ego_score = 0
     for sentence in sub_sentences:
         words = sentence.split(' ')  
@@ -24,5 +24,7 @@ def calculate_ego_score(sub_sentences):
 # gebruik:
 text = "Geachte heer/mevrouw,Ik wil graag solliciteren naar de functie van programmeur bij uw bedrijf. Ik ben de beste kandidaat voor deze functie omdat ik al jaren ervaring heb in deze branche en ik weet dat niemand anders mijn niveau van kennis en vaardigheden kan evenaren. Ik ben buitengewoon slim en in staat om snel nieuwe informatie te verwerken en te gebruiken om de beste beslissingen te nemen voor het bedrijf. Ik ben er zeker van dat ik binnen enkele weken op de hoogte zal zijn van alle zaken die spelen binnen uw bedrijf, en ik zal in staat zijn om snel resultaten te boeken en uw bedrijf naar de top te brengen. Mijn CV is overtuigend! Mijn referenties zijn heel positief over mij. Ik verdien daarom een plek in uw team. Ik kijk er naar uit om te horen wanneer ik op gesprek mag komen, zodat ik u persoonlijk kan overtuigen van mijn geschiktheid voor deze functie"
 sub_sentences = get_deelzinnen(text)
-ego_score = calculate_ego_score(sub_sentences)
+ego_score = calculate_ego_score(text)
 print(f"de ego scoren van de persoon is {ego_score}")
+
+
--- deel 5/adden opzoeken.py
+++ deel 5/adden opzoeken.py
@@ -1,22 +1,33 @@
 
+from test_lib import test, report
 # adden opzoeken
 
 def get_value(data: str, separator: str, position: int) -> str:
-    # Split de gegeven data op basis van de opgegeven separator
+    
     splitted_strings = data.split(separator)
     
-    # Controleer of de opgegeven positie geldig is
-    if 0 <= position < len(splitted_strings):
-        # Haal de waarde op die overeenkomt met de opgegeven positie
+    if 0 <= position < len(splitted_strings):   
         value = splitted_strings[position]
         return value
     else:
         return "vul een geldig ID in!"
 
 
-
+#gebruik:
 data = 'muis,kat,hond,vogel,vis'
 separator = ','
 position = 4
 result = get_value(data, separator, position)
-print(result)  
+
+expected = "muis"
+calculated = get_value(data, separator, 0)
+test('test_muis', expected, calculated)
+
+expected = "vul een geldig ID in!"
+calculated = get_value(data, separator, 16)
+test('test_muis', expected, calculated)
+
+report()
+
+
+
