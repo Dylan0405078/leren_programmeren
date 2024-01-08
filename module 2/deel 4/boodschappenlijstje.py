6b986a9f2eb7686c77fa2f0e48afeafb6216725b
dylan0405078 <99078040@mydavinci.nl>
2024-01-08 13:18:19 +0100

    gfdsg
    dsfs

--- module 2/deel 4/boodschappenlijstje.py
+++ module 2/deel 4/boodschappenlijstje.py
@@ -1,31 +1,23 @@
 #boodschappenlijstje
-def add_item(item, quantity, shopping_list):
-    item = item.lower()
-    if item in shopping_list:
-        additional_quantity = int(input("Hoeveel wil je toevoegen? "))
-        shopping_list[item] += additional_quantity
-    else:
-        shopping_list[item] = quantity
-
 def print_list(shopping_list):
-    print("- [ Boodschappenlijst ] -")
+    print("#boodschappenlijstje")
     for item, quantity in shopping_list.items():
         print(f"{quantity} x {item.capitalize()}")
     print("------------------------------------------")
 
-def main():
-    shopping_list = {}
-  
-    while True:
-        item = input("Wat wil je toevoegen aan de lijst? ")
-        quantity = int(input("Hoeveel wil je toevoegen? "))
-        add_item(item, quantity, shopping_list)
+shopping_list = {}
 
-        more = input("Wil je nog meer items toevoegen aan de lijst? (ja/nee): ")
-        if more.lower() != "ja":
-            break
+while True:
+    item = input("Wat wil je toevoegen aan de lijst? ").lower()
+    quantity = int(input("Hoeveel wil je toevoegen? "))
 
-    print_list(shopping_list)
+    if item in shopping_list:
+        shopping_list[item] += quantity
+    else:
+        shopping_list[item] = quantity
 
-if __name__ == "__main__":
-    main()
+    more = input("Wil je nog meer items toevoegen aan de lijst? (ja/nee): ")
+    if more.lower() != "ja":
+        print_list(shopping_list)
+        break
+
--- module 2/deel 4/clubbing.py
+++ module 2/deel 4/clubbing.py
@@ -1,76 +1,55 @@
-PRIJS_COLA = 1.80
-PRIJS_BIER = 2.40
-PRIJS_CHAMPAGNE = 12.30
-
-DRANKJES = ('cola', 'bier', 'champagne')
-VIP_LIST =['jeroen', 'jouke', 'rudi'] 
+# leeftijd vraag eruit halen 
+#vip lijst gebruiken
+# vip vervolg aanpassen
+#functies niet onnodig gebruiken
 
 
-
-def calculate_age_difference(age):
-    return 18 - age
-
-def ask_question(question):
-    response = input(question + " ")
-    return response
-
+# Functie voor het controleren van leeftijd
 def check_valid_age(age):
     while age < 0 or age > 150:
         age = int(input("Ongeldige leeftijd. Voer alstublieft een geldige leeftijd in: "))
     return age
 
-def main():
-    age = ask_question("Hoe oud ben je?")
-    age = check_valid_age(int(age))
 
-    if age < 18:
-        difference = calculate_age_difference(age)
-        print(f"Sorry, je bent niet oud genoeg. Probeer het over {difference} jaar.")
+# Variabelen
+name = input('Wat is je naam? ')
+age = int(input("Wat is je leeftijd? "))
+age = check_valid_age(age)
+
+
+# Constanten
+VIP_LIJST = ['Jouke', 'Jeroen', 'Rudi']
+BANDJE_ROOD = False
+BANDJE_BLAUW = False
+VIP = name.lower() in [x.lower() for x in VIP_LIJST]
+STEMPEL = age > 20
+PRIJS_COLA = 1.80
+PRIJS_BIER = 2.40
+PRIJS_CHAMPAGNE = 12.30
+
+
+# Functionaliteit clubbing
+if age < 18:
+    difference = 18 - age
+    print(f"Sorry, je bent niet oud genoeg. Probeer het over {difference} jaar.")
+else:
+    drinken = input('Wat wil je drinken? ')
+    if drinken == "champagne":
+        if not BANDJE_ROOD and not BANDJE_BLAUW and not VIP:
+            print("Sorry, champagne is alleen voor VIP.")
+        elif BANDJE_ROOD:
+            print("Je hebt een rood bandje en kunt geen champagne bestellen.")
+        else:
+            print(f"Aub, hier is je champagne dat wordt dan {PRIJS_CHAMPAGNE},-")
+    elif drinken == "cola":
+        if VIP:
+            print(f"Aub, hier heb je cola, mijn complimenten vanuit het huis!")
+        else:
+            print(f"Aub, je cola dat wordt dan {PRIJS_COLA},-")
+    elif drinken == "bier":
+        if BANDJE_BLAUW or STEMPEL or VIP:
+            print(f'Aub, hier heb je een lekkere pils, dat wordt dan {PRIJS_BIER}!')
+        elif BANDJE_ROOD or not VIP:
+            print('Je hebt een rood bandje en kunt geen bier bestellen.')
     else:
-        name = ask_question("Wat is je naam?")
-        vip = ask_question("Wil je VIP zijn? (ja/nee)")
-
-        if vip.lower() == "ja" and name.lower() in VIP_LIST:
-            over_21 = ask_question("Ben je ouder dan 21? (ja/nee)")
-
-            if over_21.lower() == "ja":
-                kleur = "blauw"
-                print(f"Je krijgt van mij een Blauw {kleur} bandje.")
-            else:
-                kleur = "rood"
-                print(f"Je krijgt van mij een Rood {kleur} bandje.")
-        else:
-            over_21 = ask_question("Ben je ouder dan 21? (ja/nee)")
-
-            if over_21.lower() == "ja":
-                print("Je krijgt een blauw bandje.")
-            else:
-                print("Je krijgt een rood bandje.")
-
-        drink = ask_question("Wat wil je drinken?")
-
-        if drink.lower() == "cola":
-            bandje = ask_question("Heb je een bandje? (ja/nee)")
-
-            if bandje.lower() == "ja":
-                print("Astublieft, complimenten van het huis!")
-            else:
-                print(f"Asjeblieft, je {drink}. Dat is dan {PRIJS_COLA} euro.")
-        elif drink.lower() == "bier":
-            stempel_bandje = ask_question("Heb je een stempel of bandje? (ja/nee)")
-
-            if stempel_bandje.lower() == "ja":
-                print(f"Asjeblieft, je {drink}. Dat is dan {PRIJS_BIER} euro.")
-            else:
-                print("Sorry, je mag nog geen alcohol bestellen onder de 21.")
-        elif drink.lower() == "champagne":
-            if vip.lower() == "ja" and name.lower() in VIP_LIST:
-                print(f"Astublieft, je {drink}. Dat is dan {PRIJS_CHAMPAGNE} euro.")
-            else:
-                print("Sorry, alleen VIPs mogen champagne bestellen.")
-        else:
-            kleur = "rood"
-            print(f"Geen idee wat je bedoelt. Hier heb je een glaasje water. Je krijgt van mij een rood {kleur} bandje.")
-
-if __name__ == "__main__":
-    main()
+        print('Ik weet niet wat je bedoelt, hier heb je een glas water.')
